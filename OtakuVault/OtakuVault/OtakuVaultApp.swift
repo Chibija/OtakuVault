7d2b6318a70c934c530b8c8be42c31cc03c52772
Michael Bosworth <mbosworth.dev@gmail.com>
2023-05-14 22:55:43 -0600

    :tada: Initial commit

---
+++ OtakuVault/OtakuVault/OtakuVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtakuVaultApp.swift
+//  OtakuVault
+//
+//  Created by Michael Bosworth on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct OtakuVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
